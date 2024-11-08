c451c5a35677047c27e912765462a5c4a3e22259
Valentijn Hol <git@hirtol.top>
2024-11-08 23:58:10 +0100

    Basic PoC

---
+++ small_talk_ml/src/embeddings.rs
@@ -0,0 +1,225 @@
+use std::path::Path;
+use eyre::Context;
+use llama_cpp_2::context::LlamaContext;
+use llama_cpp_2::context::params::LlamaContextParams;
+use llama_cpp_2::llama_backend::LlamaBackend;
+use llama_cpp_2::llama_batch::LlamaBatch;
+use llama_cpp_2::model::{AddBos, LlamaModel};
+use llama_cpp_2::model::params::LlamaModelParams;
+use llama_cpp_2::token::LlamaToken;
+
+self_cell::self_cell!(
+    pub struct LlamaCell {
+        owner: LlamaModel,
+        #[covariant]
+        dependent: LlamaContext,
+    }
+);
+
+pub struct LLamaEmbedder {
+    backend: LlamaBackend,
+    model: LlamaCell,
+    batch: LlamaBatch,
+}
+
+impl LLamaEmbedder {
+    /// Create a [LLamaEmbedder] by initialising the `LLama.CPP` backend and loading the given model from `model_path`.
+    ///
+    /// # Arguments
+    /// * `model_path` - The location of the GGUF model to be loaded
+    /// * `model_params` - The parameters for the `LLama.CPP` model
+    /// * `ctx_params` - The parameters for the context
+    /// * `n_batch` - If one wants to forcefully limit the amount of tokens then provide [Some], otherwise just leave it to [None]
+    /// to use the default `n_ctx` of the model as the max.
+    pub fn new(
+        model_path: impl AsRef<Path>,
+        model_params: LlamaModelParams,
+        ctx_params: LlamaContextParams,
+        n_batch: Option<u32>,
+    ) -> eyre::Result<Self> {
+        let mut backend = LlamaBackend::init()?;
+        backend.void_logs();
+
+        let model = LlamaModel::load_from_file(&backend, model_path, &model_params)?;
+
+        let ctx_params = ctx_params.with_embeddings(true);
+        let ctx_params = if let Some(n_batch) = n_batch {
+            ctx_params.with_n_batch(n_batch)
+        } else {
+            ctx_params
+        };
+        let cell = LlamaCell::try_new(model, |model| {
+            model
+                .new_context(&backend, ctx_params)
+                .context("unable to create the llama_context")
+        })?;
+
+        let n_ctx = cell.borrow_dependent().n_ctx();
+        let n_train = cell.borrow_owner().n_ctx_train();
+        if n_train < n_ctx {
+            eyre::bail!("Given context's `n_ctx` (`{n_ctx}`) was larger than the model was trained for (`{n_train}`).")
+        }
+
+        let batch = LlamaBatch::new(n_batch.unwrap_or(n_ctx) as usize, 1);
+
+        Ok(Self {
+            backend,
+            model: cell,
+            batch,
+        })
+    }
+
+    /// Tokenize the given texts using the default `LLama.CPP` tokenizer for the current model.
+    pub fn tokenize(
+        &self,
+        texts: impl IntoIterator<Item = impl AsRef<str>>,
+    ) -> eyre::Result<Vec<Vec<LlamaToken>>> {
+        texts
+            .into_iter()
+            .map(|text| {
+                self.model
+                    .borrow_owner()
+                    .str_to_token(text.as_ref(), AddBos::Always)
+                    .context("Failed to tokenize")
+            })
+            .collect()
+    }
+
+    pub fn embed_tokens(
+        &mut self,
+        batch_tokens: &mut [Vec<LlamaToken>],
+        normalise: bool,
+        truncate: bool,
+    ) -> eyre::Result<Vec<Vec<f32>>> {
+        let mut output = Vec::with_capacity(batch_tokens.len());
+
+        let mut max_seq_id = 0;
+        let n_batch = self.model.borrow_dependent().n_batch() as usize;
+
+        for tokens in batch_tokens {
+            // Force the prompt to be at most the size of the context
+            if truncate {
+                tokens.truncate(n_batch);
+            } else if tokens.len() > n_batch {
+                eyre::bail!(
+                    "The given tokens produces `{}` tokens when at most `{}` are allowed in a batch",
+                    tokens.len(),
+                    n_batch
+                );
+            }
+
+            // Batch has been filled up
+            if (self.batch.n_tokens() as usize + tokens.len()) > n_batch {
+                self.decode_batch(&mut output, max_seq_id, normalise);
+                max_seq_id = 0;
+            }
+
+            self.batch.add_sequence(tokens, max_seq_id as i32, false)?;
+            max_seq_id += 1;
+        }
+
+        // Handle last batch
+        self.decode_batch(&mut output, max_seq_id, normalise);
+
+        Ok(output)
+    }
+
+    /// Create embeddings of the given text sequences in a batched form.
+    ///
+    /// # Arguments
+    /// * `texts` - The texts to create embeddings for
+    /// * `normal` - How the embeddings should be normalised
+    /// * `truncate` - Whether to truncate any of the given texts if they produce more tokens than the context window of the model can handle.
+    /// If this is not set to `true` then the function will return with an error instead.
+    pub fn embed(
+        &mut self,
+        texts: impl IntoIterator<Item = impl AsRef<str>>,
+        normalise: bool,
+        truncate: bool,
+    ) -> eyre::Result<Vec<Vec<f32>>> {
+        let texts = texts.into_iter();
+        let mut output = Vec::with_capacity(texts.size_hint().1.unwrap_or(0));
+
+        let mut t_batch = 0;
+        let mut p_batch = 0;
+        let n_batch = self.model.borrow_dependent().n_batch() as usize;
+
+        for text in texts {
+            let mut tokens = self
+                .model
+                .borrow_owner()
+                .str_to_token(text.as_ref(), AddBos::Always)?;
+
+            // Force the prompt to be at most the size of the context
+            if truncate {
+                tokens.truncate(n_batch);
+            } else if tokens.len() > n_batch {
+                eyre::bail!(
+                    "The given text: `{}` produces `{}` tokens when at most `{}` are allowed in a batch",
+                    text.as_ref(),
+                    tokens.len(),
+                    n_batch
+                );
+            }
+
+            let n_tokens = tokens.len();
+
+            // Batch has been filled up
+            if t_batch + n_tokens > n_batch {
+                self.decode_batch(&mut output, p_batch, normalise);
+                t_batch = 0;
+                p_batch = 0;
+            }
+
+            self.batch.add_sequence(&tokens, p_batch as i32, false)?;
+            t_batch += n_tokens;
+            p_batch += 1;
+        }
+
+        // Handle last batch
+        self.decode_batch(&mut output, p_batch, normalise);
+
+        Ok(output)
+    }
+
+    fn decode_batch(&mut self, output: &mut Vec<Vec<f32>>, n_sequence: usize, normalise: bool) {
+        self.model.with_dependent_mut(|_, ctx| {
+            ctx.clear_kv_cache();
+            let _ = ctx.decode(&mut self.batch);
+
+            for i in 0..n_sequence {
+                let embedding = ctx
+                    .embeddings_seq_ith(i as i32)
+                    .expect("Embeddings are always enabled");
+                // Either normalise immediately, or perform mean-pooling.
+                let embedding = if normalise {
+                    normalize(embedding)
+                } else {
+                    embedding.to_vec()
+                };
+
+                output.push(embedding);
+            }
+
+            self.batch.clear();
+        });
+    }
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord, Hash)]
+pub enum Normalisation {
+    /// Normalise the embeddings, ensuring an efficient cosine similarity can be computed
+    Normalise,
+    /// Only use mean pooling on the output embedding.
+    None,
+}
+
+pub fn normalize(vec: &[f32]) -> Vec<f32> {
+    let magnitude = (vec.iter().fold(0.0, |acc, &val| val.mul_add(val, acc))).sqrt();
+
+    if magnitude > f32::EPSILON {
+        vec.iter().map(|&val| val / magnitude).collect()
+    } else {
+        vec.to_vec()
+    }
+}
--- small_talk_ml/src/main.rs
+++ small_talk_ml/src/main.rs
@@ -1,3 +1,137 @@
-fn main() {
+use burn::{
+    backend::Wgpu,
+    data::{
+        dataloader::batcher::Batcher,
+        dataset::{Dataset, HuggingfaceDatasetLoader, SqliteDataset},
+    },
+    prelude::{Backend, ElementConversion, Int, TensorData},
+    tensor::Tensor,
+};
+use llama_cpp_2::model::params::LlamaModelParams;
+use serde::{Deserialize, Serialize};
+use small_talk_ml::{embeddings::LLamaEmbedder, EmotionModelConfig};
+use std::cell::RefCell;
+use std::cmp::Reverse;
+use std::sync::{Arc, Mutex};
+use std::time::Instant;
+use burn::backend::Autodiff;
+use burn::optim::AdamConfig;
+use burn::prelude::{Config, Module};
+use burn::record::{CompactRecorder, Recorder};
+use burn::tensor::activation::softmax;
+use burn::tensor::cast::ToElement;
+use itertools::Itertools;
+use llama_cpp_2::context::params::LlamaContextParams;
+use small_talk_ml::training::{GoEmotionItem, TrainingConfig};
 
+const CLASSES: [&str; 28] = [
+    "admiration",
+    "amusement",
+    "anger",
+    "annoyance",
+    "approval",
+    "caring",
+    "confusion",
+    "curiosity",
+    "desire",
+    "disappointment",
+    "disapproval",
+    "disgust",
+    "embarrassment",
+    "excitement",
+    "fear",
+    "gratitude",
+    "grief",
+    "joy",
+    "love",
+    "nervousness",
+    "optimism",
+    "pride",
+    "realization",
+    "relief",
+    "remorse",
+    "sadness",
+    "surprise",
+    "neutral",
+];
+
+type Back = Wgpu<f32, i32>;
+type MyAuto = Autodiff<Back>;
+
+fn main() -> eyre::Result<()> {
+    let device = Default::default();
+    let model_params = LlamaModelParams::default().with_n_gpu_layers(0);
+    let ctx_params = LlamaContextParams::default()
+        .with_n_threads(16)
+        .with_n_threads_batch(16)
+        .with_n_ctx(None) // Load from model
+        .with_n_batch(512)
+        .with_embeddings(true);
+    let mut llama = LLamaEmbedder::new(
+        "G:\\ML Models\\Embedding Models\\bge-small-en-v1.5\\ggml-model-Q4_k.gguf",
+        model_params,
+        ctx_params,
+        None,
+    )?;
+    let model = EmotionModelConfig::new(384, 28).init::<Back>(&device);
+    let model_cfg = EmotionModelConfig::new(384, 28);
+
+    let train: SqliteDataset<GoEmotionItem> =
+        HuggingfaceDatasetLoader::new("google-research-datasets/go_emotions").dataset("train")?;
+    let test: SqliteDataset<GoEmotionItem> =
+        HuggingfaceDatasetLoader::new("google-research-datasets/go_emotions").dataset("test")?;
+
+    println!("Model: {model:?}");
+    println!("Data: {:?}", train.get(0));
+
+    let now = Instant::now();
+
+    let embeddings = llama.embed(["\"Johny, why are you going over there? Johny?!\"", "Oh my clowns, great... I'm going to fucking kill you"], false, true)?;
+    let emb_shape = [embeddings[0].len()];
+
+    let mut embeddings2 = embeddings
+        .into_iter()
+        .map(|emb| TensorData::new(emb, emb_shape).convert::<f32>())
+        .map(|data| Tensor::<Back, 1>::from_data(data, &device))
+        .map(|tens| tens.reshape([1, emb_shape[0]]))
+        .collect::<Vec<_>>();
+    let embeddings = Tensor::cat(embeddings2, 0).to_device(&device);
+    infer("./artifacts", device, embeddings);
+    // let logits = model.forward(embeddings2.pop().unwrap());
+    // println!("Logits: {logits:?}");
+
+    // small_talk_ml::training::train::<MyAuto>("./artifacts", train, test, llama, TrainingConfig::new(model_cfg, AdamConfig::new()), device);
+
+    println!("Training took: {:?}", now.elapsed());
+    Ok(())
+}
+
+pub fn infer<B: Backend>(artifact_dir: &str, device: B::Device, embedding: Tensor<B, 2>) {
+    let config = TrainingConfig::load(format!("{artifact_dir}/config.json"))
+        .expect("Config should exist for the model");
+    let record = CompactRecorder::new()
+        .load(format!("{artifact_dir}/model").into(), &device)
+        .expect("Trained model should exist");
+
+    let model = config.model.init::<B>(&device).load_record(record);
+
+    // let label = item.label;
+    // let batcher = MnistBatcher::new(device);
+    // let batch = batcher.batch(vec![item]);
+    let output = model.forward(embedding);
+    
+    let probabilities = softmax(output.clone(), 1);
+    for slice in probabilities.iter_dim(0) {
+        let tensor_data = slice.into_data();
+        let data = tensor_data.as_slice::<f32>().unwrap();
+        let top_5 = data.iter().enumerate().map(|(i, prob)| (CLASSES[i], prob)).sorted_by(|a, b| b.1.partial_cmp(a.1).unwrap()).take(5).collect_vec();
+        
+        println!("Slice: {top_5:#?}");
+    }
+    
+    println!("Output: {}", output.clone().argmax(1).flatten::<1>(0, 1));
+    println!("Output Soft: {}", softmax(output.clone(), 1).into_data());
+    let predicted = output.argmax(1).flatten::<1>(0, 1).into_scalar();
+
+    println!("Predicted {} = {}", predicted, CLASSES[predicted.to_usize()]);
 }
