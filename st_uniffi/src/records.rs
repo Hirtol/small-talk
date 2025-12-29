1c24fdb5a37eab53ab9a730edb852573d0566115
Hirtol <git@hirtol.top>
2025-12-29 22:16:13 +0100

    Add more methods/records

--- st_uniffi/src/records.rs
+++ st_uniffi/src/records.rs
@@ -1,4 +1,4 @@
-use st_system::voice_manager::{FsVoice, FsVoiceData};
+use std::path::PathBuf;
 
 #[derive(uniffi::Record, Clone, Debug)]
 pub struct VoiceReference {
@@ -15,17 +15,347 @@ impl From<st_system::voice_manager::VoiceReference> for VoiceReference {
     }
 }
 
+impl From<VoiceReference> for st_system::voice_manager::VoiceReference {
+    fn from(value: VoiceReference) -> Self {
+        Self {
+            name: value.name,
+            location: value.location.into(),
+        }
+    }
+}
+
 #[derive(uniffi::Enum, Clone, Debug)]
 pub enum VoiceDestination {
     Global,
-    Game(String),
+    Game { name: String },
 }
 
 impl From<st_system::voice_manager::VoiceDestination> for VoiceDestination {
     fn from(value: st_system::voice_manager::VoiceDestination) -> Self {
         match value {
             st_system::voice_manager::VoiceDestination::Global => Self::Global,
-            st_system::voice_manager::VoiceDestination::Game(game) => Self::Game(game),
+            st_system::voice_manager::VoiceDestination::Game(game) => Self::Game { name: game },
+        }
+    }
+}
+
+impl From<VoiceDestination> for st_system::voice_manager::VoiceDestination {
+    fn from(value: VoiceDestination) -> Self {
+        match value {
+            VoiceDestination::Global => st_system::voice_manager::VoiceDestination::Global,
+            VoiceDestination::Game { name } => st_system::voice_manager::VoiceDestination::Game(name)
+        }
+    }
+}
+
+#[derive(uniffi::Record, Clone, Debug)]
+pub struct VoiceLine {
+    pub line: String,
+    pub person: TtsVoice,
+    pub model: TtsModel,
+    pub force_generate: bool,
+    pub post: Option<PostProcessing>,
+}
+
+impl From<st_system::data::VoiceLine> for VoiceLine {
+    fn from(value: st_system::data::VoiceLine) -> Self {
+        Self {
+            line: value.line,
+            person: value.person.into(),
+            model: value.model.into(),
+            force_generate: value.force_generate,
+            post: value.post.map(|p| p.into()),
+        }
+    }
+}
+
+impl From<VoiceLine> for st_system::data::VoiceLine {
+    fn from(value: VoiceLine) -> Self {
+        Self {
+            line: value.line,
+            person: value.person.into(),
+            model: value.model.into(),
+            force_generate: value.force_generate,
+            post: value.post.map(|p| p.into()),
+        }
+    }
+}
+
+#[derive(uniffi::Record, Clone, Debug)]
+pub struct TtsResponse {
+    pub file_path: String,
+    pub line: String,
+    pub voice_used: VoiceReference,
+}
+
+impl From<st_system::data::TtsResponse> for TtsResponse {
+    fn from(value: st_system::data::TtsResponse) -> Self {
+        Self {
+            file_path: value.file_path.to_string_lossy().to_string(),
+            line: value.line,
+            voice_used: value.voice_used.into(),
+        }
+    }
+}
+
+impl From<TtsResponse> for st_system::data::TtsResponse {
+    fn from(value: TtsResponse) -> Self {
+        Self {
+            file_path: PathBuf::from(value.file_path),
+            line: value.line,
+            voice_used: value.voice_used.into(),
+        }
+    }
+}
+
+#[derive(uniffi::Enum, Clone, Debug)]
+pub enum TtsVoice {
+    ForceVoice(VoiceReference),
+    CharacterVoice(CharacterVoice),
+}
+
+impl From<st_system::data::TtsVoice> for TtsVoice {
+    fn from(value: st_system::data::TtsVoice) -> Self {
+        match value {
+            st_system::data::TtsVoice::ForceVoice(voice_ref) => Self::ForceVoice(voice_ref.into()),
+            st_system::data::TtsVoice::CharacterVoice(char) => Self::CharacterVoice(char.into()),
+        }
+    }
+}
+
+impl From<TtsVoice> for st_system::data::TtsVoice {
+    fn from(value: TtsVoice) -> Self {
+        match value {
+            TtsVoice::ForceVoice(voice_ref) => Self::ForceVoice(voice_ref.into()),
+            TtsVoice::CharacterVoice(char) => Self::CharacterVoice(char.into()),
+        }
+    }
+}
+
+#[derive(uniffi::Record, Clone, Debug)]
+pub struct PostProcessing {
+    pub verify_percentage: Option<u8>,
+    pub trim_silence: bool,
+    pub normalise: bool,
+    pub rvc: Option<RvcOptions>,
+}
+
+impl From<st_system::data::PostProcessing> for PostProcessing {
+    fn from(value: st_system::data::PostProcessing) -> Self {
+        Self {
+            verify_percentage: value.verify_percentage,
+            trim_silence: value.trim_silence,
+            normalise: value.normalise,
+            rvc: value.rvc.map(|r| r.into()),
+        }
+    }
+}
+
+impl From<PostProcessing> for st_system::data::PostProcessing {
+    fn from(value: PostProcessing) -> Self {
+        Self {
+            verify_percentage: value.verify_percentage,
+            trim_silence: value.trim_silence,
+            normalise: value.normalise,
+            rvc: value.rvc.map(|r| r.into()),
+        }
+    }
+}
+
+#[derive(uniffi::Record, Clone, Debug)]
+pub struct RvcOptions {
+    pub model: RvcModel,
+    pub high_quality: bool,
+}
+
+impl From<st_system::data::RvcOptions> for RvcOptions {
+    fn from(value: st_system::data::RvcOptions) -> Self {
+        Self {
+            model: value.model.into(),
+            high_quality: value.high_quality,
+        }
+    }
+}
+
+impl From<RvcOptions> for st_system::data::RvcOptions {
+    fn from(value: RvcOptions) -> Self {
+        Self {
+            model: value.model.into(),
+            high_quality: value.high_quality,
+        }
+    }
+}
+
+#[derive(uniffi::Enum, Clone, Debug)]
+pub enum RvcModel {
+    SeedVc,
+}
+
+impl From<st_system::data::RvcModel> for RvcModel {
+    fn from(value: st_system::data::RvcModel) -> Self {
+        match value {
+            st_system::data::RvcModel::SeedVc => Self::SeedVc,
+        }
+    }
+}
+
+impl From<RvcModel> for st_system::data::RvcModel {
+    fn from(value: RvcModel) -> Self {
+        match value {
+            RvcModel::SeedVc => Self::SeedVc,
+        }
+    }
+}
+
+#[derive(uniffi::Enum, Clone, Debug)]
+pub enum TtsModel {
+    Xtts,
+    IndexTts,
+    EchoTts,
+}
+
+impl From<st_system::data::TtsModel> for TtsModel {
+    fn from(value: st_system::data::TtsModel) -> Self {
+        match value {
+            st_system::data::TtsModel::Xtts => Self::Xtts,
+            st_system::data::TtsModel::IndexTts => Self::IndexTts,
+            st_system::data::TtsModel::EchoTts => Self::EchoTts,
+        }
+    }
+}
+
+impl From<TtsModel> for st_system::data::TtsModel {
+    fn from(value: TtsModel) -> Self {
+        match value {
+            TtsModel::Xtts => st_system::data::TtsModel::Xtts,
+            TtsModel::IndexTts => Self::IndexTts,
+            TtsModel::EchoTts => Self::EchoTts,
+        }
+    }
+}
+
+#[derive(uniffi::Enum, Clone, Debug, Copy)]
+pub enum PlaybackEnvironment {
+    Outdoors,
+    Indoors,
+    Cave,
+}
+
+impl From<st_system::audio::playback::PlaybackEnvironment> for PlaybackEnvironment {
+    fn from(value: st_system::audio::playback::PlaybackEnvironment) -> Self {
+        match value {
+            st_system::audio::playback::PlaybackEnvironment::Outdoors => Self::Outdoors,
+            st_system::audio::playback::PlaybackEnvironment::Indoors => Self::Indoors,
+            st_system::audio::playback::PlaybackEnvironment::Cave => Self::Cave,
+        }
+    }
+}
+
+impl From<PlaybackEnvironment> for st_system::audio::playback::PlaybackEnvironment {
+    fn from(value: PlaybackEnvironment) -> Self {
+        match value {
+            PlaybackEnvironment::Outdoors => st_system::audio::playback::PlaybackEnvironment::Outdoors,
+            PlaybackEnvironment::Indoors => st_system::audio::playback::PlaybackEnvironment::Indoors,
+            PlaybackEnvironment::Cave => st_system::audio::playback::PlaybackEnvironment::Cave,
+        }
+    }
+}
+
+#[derive(uniffi::Record, Clone, Debug)]
+pub struct PlaybackSettings {
+    pub environment: Option<PlaybackEnvironment>,
+    pub volume: Option<f32>,
+    pub speed: Option<f64>,
+}
+
+impl From<st_system::audio::playback::PlaybackSettings> for PlaybackSettings {
+    fn from(value: st_system::audio::playback::PlaybackSettings) -> Self {
+        Self {
+            environment: value.environment.map(|e| e.into()),
+            volume: value.volume,
+            speed: value.speed,
+        }
+    }
+}
+
+impl From<PlaybackSettings> for st_system::audio::playback::PlaybackSettings {
+    fn from(value: PlaybackSettings) -> Self {
+        Self {
+            environment: value.environment.map(|e| e.into()),
+            volume: value.volume,
+            speed: value.speed,
+        }
+    }
+}
+
+#[derive(uniffi::Record, Clone, Debug)]
+pub struct PlaybackVoiceLine {
+    pub request: VoiceLine,
+    pub playback: Option<PlaybackSettings>,
+}
+
+impl From<st_system::audio::playback::PlaybackVoiceLine> for PlaybackVoiceLine {
+    fn from(value: st_system::audio::playback::PlaybackVoiceLine) -> Self {
+        Self {
+            request: value.line.into(),
+            playback: value.playback.map(|p| p.into()),
+        }
+    }
+}
+
+impl From<PlaybackVoiceLine> for st_system::audio::playback::PlaybackVoiceLine {
+    fn from(value: PlaybackVoiceLine) -> Self {
+        Self {
+            line: value.request.into(),
+            playback: value.playback.map(|p| p.into()),
+        }
+    }
+}
+
+#[derive(uniffi::Record, Clone, Debug, Hash, PartialEq, Eq)]
+pub struct CharacterVoice {
+    pub name: String,
+    pub gender: Option<Gender>,
+}
+
+impl From<st_system::CharacterVoice> for CharacterVoice {
+    fn from(value: st_system::CharacterVoice) -> Self {
+        Self {
+            name: value.name,
+            gender: value.gender.map(|i| i.into()),
+        }
+    }
+}
+
+impl From<CharacterVoice> for st_system::data::CharacterVoice {
+    fn from(value: CharacterVoice) -> Self {
+        Self {
+            name: value.name,
+            gender: value.gender.map(|i| i.into()),
+        }
+    }
+}
+
+#[derive(uniffi::Enum, Debug, Clone, Copy, Hash, PartialEq, Eq)]
+pub enum Gender {
+    Male,
+    Female,
+}
+
+impl From<st_system::Gender> for Gender {
+    fn from(value: st_system::Gender) -> Self {
+        match value {
+            st_system::Gender::Male => Gender::Male,
+            st_system::Gender::Female => Gender::Female,
+        }
+    }
+}
+
+impl From<Gender> for st_system::Gender {
+    fn from(value: Gender) -> Self {
+        match value {
+            Gender::Male => st_system::Gender::Male,
+            Gender::Female => st_system::Gender::Female,
         }
     }
 }
--- st_uniffi/src/session.rs
+++ st_uniffi/src/session.rs
@@ -1,5 +1,9 @@
-use crate::{records::VoiceReference, system::StSystemFfi};
+use crate::{
+    records::{CharacterVoice, PlaybackVoiceLine, TtsResponse, VoiceLine, VoiceReference},
+    system::StSystemFfi,
+};
 use st_system::session::GameSessionHandle;
+use std::collections::HashMap;
 
 #[derive(uniffi::Object, Clone)]
 pub struct StGameSessionFfi {
@@ -17,5 +21,57 @@ impl StGameSessionFfi {
             .into_iter()
             .map(|i| i.reference.into())
             .collect())
+    }
+
+    pub async fn session_characters(&self) -> crate::Result<HashMap<CharacterVoice, VoiceReference>> {
+        let output = self.handle.character_voices().await?;
+
+        Ok(output.into_iter().map(|(k, v)| (k.into(), v.into())).collect())
+    }
+
+    pub async fn set_session_characters(
+        &self,
+        characters: HashMap<CharacterVoice, VoiceReference>,
+    ) -> crate::Result<()> {
+        for (character, voice) in characters {
+            self.handle
+                .force_character_voice(character.into(), voice.into())
+                .await?;
+        }
+        Ok(())
+    }
+
+    pub async fn tts_playback_start(&self, requests: Vec<PlaybackVoiceLine>) -> crate::Result<()> {
+        self.handle
+            .playback
+            .start(requests.into_iter().map(|i| i.into()).collect())
+            .await?;
+
+        Ok(())
+    }
+
+    pub async fn tts_playback_speed(&self, speed: f64) -> crate::Result<()> {
+        self.handle.playback.set_speed(speed).await?;
+
+        Ok(())
+    }
+
+    pub async fn tts_playback_stop(&self) -> crate::Result<()> {
+        self.handle.playback.stop().await?;
+        Ok(())
+    }
+
+    pub async fn tts_request(&self, request: VoiceLine) -> crate::Result<TtsResponse> {
+        let out = self.handle.request_tts(request.into()).await?;
+
+        Ok(st_system::TtsResponse::clone(&out).into())
+    }
+
+    pub async fn tts_queue(&self, requests: Vec<VoiceLine>) -> crate::Result<()> {
+        self.handle
+            .add_all_to_queue(requests.into_iter().map(|i| i.into()).collect())
+            .await?;
+
+        Ok(())
     }
 }
