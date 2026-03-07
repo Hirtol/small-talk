27fa3fc4bf8321bd6bb0221ac29af24744e7951d
Hirtol <git@hirtol.top>
2026-03-07 19:18:12 +0100

    Fix organiser not loading after config changes

--- st_organiser/src/main.rs
+++ st_organiser/src/main.rs
@@ -1,16 +1,23 @@
 use std::sync::Arc;
 use clap::Parser;
 use tracing_subscriber::util::SubscriberInitExt;
+use st_application::config::SmallTalkConfig;
 use crate::args::SubCommands;
 
 mod args;
 mod trace;
 
+#[derive(serde::Serialize, serde::Deserialize, Clone, Debug, Default)]
+pub struct SmallTalkOrganiserConfig {
+    #[serde(default)]
+    pub small_talk: SmallTalkConfig,
+}
+
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
     color_eyre::install()?;
     let args = args::ClapArgs::parse();
-    let conf = Arc::new(st_application::config::initialise_config()?);
+    let conf = Arc::new(st_application::config::initialise_config::<SmallTalkOrganiserConfig>()?.small_talk);
     trace::create_subscriber("ERROR,st_http=TRACE,st_organiser=TRACE,st_ml=TRACE,st_system=TRACE").init();
 
     let now = std::time::Instant::now();
